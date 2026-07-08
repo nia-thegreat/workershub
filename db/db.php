037f39eee5d18a932490cbf580c7d522a1d09b87
nia_rania <niathegreat6078@gmail.com>
2026-07-08 12:21:20 +0530

    Remove database credentials

--- db/db.php
+++ db/db.php
@@ -5,11 +5,6 @@ $user = "YOUR_DB_USER";
 $password = "YOUR_DB_PASSWORD";
 $database = "YOUR_DB_NAME";
 
-$host = "YOUR_DB_HOST";
-$user = "YOUR_DB_USER";
-$password = "YOUR_DB_PASSWORD";
-$database = "YOUR_DB_NAME";
-
 $conn = new mysqli($host, $user, $password, $database );
 
 if ($conn->connect_error) {
